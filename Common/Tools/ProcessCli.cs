379fb30706efac986acee905cbce890d1c539106
Rob Smyth <RobSmyth@users.noreply.github.com>
2024-11-12 13:55:41 +1100

    Testing

--- Common/Tools/ProcessCli.cs
+++ Common/Tools/ProcessCli.cs
@@ -80,7 +80,7 @@ public sealed class ProcessCli : IProcessCli
         if (completed)
         {
             System.Threading.Thread.SpinWait(2);
-            System.Threading.Thread.Sleep(300);
+            System.Threading.Thread.Sleep(1);
             //process.WaitForExit();
         }
 
